aac527c3a5ec83b4e7b465fd824b474fd6fdeb11
erlm <erlm@kea.dk>
2024-09-11 11:34:29 +0200

    nu med env var

--- src/test/java/org/example/jpastudent23a/repositories/StudentRepositoryTest.java
+++ src/test/java/org/example/jpastudent23a/repositories/StudentRepositoryTest.java
@@ -1,16 +1,35 @@
 package org.example.jpastudent23a.repositories;
 
+import org.example.jpastudent23a.model.Student;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
 
-@SpringBootTest
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+@DataJpaTest
 class StudentRepositoryTest {
 
 
-    @Test
-    void contextLoads() {
+    @Autowired
+    StudentRepository studentRepository;
 
+    @Test
+    void testAtLeastOneErik() {
+        List<Student> std = studentRepository.findAllByName("Erik");
+        //assertEquals(1, std.size());
+        assertTrue(std.size()==0);
+    }
+
+    @Test
+    void testOneErik() {
+        List<Student> std = studentRepository.findAllByName("Erik");
+        assertEquals(0, std.size());
     }
 
 }
