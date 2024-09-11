eb5b2bcebf099162fac7657c8b437f8705a5e84a
erlm <erlm@kea.dk>
2024-09-11 10:23:52 +0200

    nu uden test

--- src/test/java/org/example/jpastudent23a/repositories/StudentRepositoryTest.java
+++ src/test/java/org/example/jpastudent23a/repositories/StudentRepositoryTest.java
@@ -1,31 +1,16 @@
 package org.example.jpastudent23a.repositories;
 
-import org.example.jpastudent23a.model.Student;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class StudentRepositoryTest {
 
-    @Autowired
-    StudentRepository studentRepository;
 
     @Test
-    void testAtLeastOneErik() {
-        List<Student> std = studentRepository.findAllByName("Erik");
-        //assertEquals(1, std.size());
-        assertTrue(std.size()==0);
-    }
+    void contextLoads() {
 
-    @Test
-    void testOneErik() {
-        List<Student> std = studentRepository.findAllByName("Erik");
-        assertEquals(0, std.size());
     }
 
 }
